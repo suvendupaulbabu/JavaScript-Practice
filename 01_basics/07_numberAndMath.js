96d6ce8eaab9c3dac79556c67bfa33fbe554a63e
suvendupaulbabu <suvendupaulbabu@gmail.com>
2024-08-12 07:50:05 +0000

    Number And Math Notes

---
+++ 01_basics/07_numberAndMath.js
@@ -0,0 +1,7 @@
+const score = 400;
+const balance = new Number(score) // Using new Number() it's convert to object
+//console.log(balance);
+const newBalance = balance
+console.log(newBalance.toString());
+console.log(newBalance.toString().length);
+console.log(typeof newBalance.toString());
