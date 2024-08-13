7886df9b90a0b8012486ddfc47d81ed19f69165a
suvendupaulbabu <suvendupaulbabu@gmail.com>
2024-08-13 07:09:07 +0000

    Array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,23 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+const myCities = ["Barasat", "Madhyamgram"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+//console.log(myArr[0]);
+
+// Array methods
+//myArr.push(6) // Using push() method insert an element in the array (It's insert last element in an array)
+//myArr.pop() // Remove last element from array
+//myArr.unshift(9) // Using unshift() method insert element at the first an array
+//myArr.shift() // Using shift() method remove array element from 1st an array
+
+//console.log(myArr.includes(9)); // Uaing includes() method return true/false based on search in an array elements
+//console.log(myArr.indexOf(5)); // It's return index value of an array but if mention value/element is not find then it's return -1
+
+//const newArray = myArr.join() // Using join() method convert and bind array to string
+//console.log(newArray);
+//console.log(typeof newArray);
+
+// slice, splice
+console.log("A", myArr)
+
+const myNewArr1 = myArr.slice()
