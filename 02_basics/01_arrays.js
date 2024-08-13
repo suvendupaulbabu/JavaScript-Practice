f7069674906cc541804a1d289a22bb9bf135be9a
suvendupaulbabu <suvendupaulbabu@gmail.com>
2024-08-13 07:53:47 +0000

    Array Notes

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -18,6 +18,17 @@ const myArr2 = new Array(1, 2, 3, 4)
 //console.log(typeof newArray);
 
 // slice, splice
-console.log("A", myArr)
 
-const myNewArr1 = myArr.slice()
+
+const myNewArr1 = myArr.slice(1, 3) // slice() method working two value, first one is set from array index and 2nd one up-to end index, like myArr 1st index have 0 2nd index have and 4th have 3
+//console.log("A", myArr)
+//console.log("B", myArr);
+
+console.log("Original Array", myArr)
+
+console.log('slice', myNewArr1)
+console.log("B", myArr)
+
+const myNewArr2 = myArr.splice(1, 3)
+console.log('splice', myNewArr2)
+console.log("C", myArr)
