eaa22ff9b9ea7c18797f4e5974be4a42c9b2b864
suvendupaulbabu <suvendupaulbabu@gmail.com>
2024-08-16 11:03:54 +0000

    Object Note

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -26,4 +26,37 @@ const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "c", 4: "d"}
 //const obj3 = {...obj1, ...obj2} // Asign multiple object in one object using "spread" oparetor
 const obj3 = Object.assign({}, obj1, obj2) // Using Object.assign() method first curly bracket{} is "target" and object name is source
-console.log(obj3);
+//console.log(obj3);
+
+// Array of Object
+const arrOBJ = [
+    {
+        id: 1,
+        email: "test01@gmail.com", 
+        isLoggedIN : false
+    }, 
+    {
+        id: 2,
+        email: "test02@gmail.com", 
+        isLoggedIN : false
+    },
+    {
+        id: 3,
+        email: "test03@gmail.com", 
+        isLoggedIN : true
+    }
+]
+
+const fetchArrObj = arrOBJ.map((item) =>{
+    //console.log(item);
+    //console.log(Object.keys(item));
+    //console.log(Object.values(item));
+    //console.log(Object.values(item).length);
+    console.log(Object.entries(item));
+})
+
+//console.log(arrOBJ[0]);
+//console.log(Object.keys(arrOBJ[0]));
+//console.log(Object.values(arrOBJ[0]));
+//console.log(Object.hasOwn(arrOBJ[0], 'isLoggedIN'))
+//console.log(arrOBJ[0].hasOwnProperty('isLoggedIN'))
