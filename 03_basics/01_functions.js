6c65dec98019ef93cb0e588df0de0a1012d209f4
suvendupaulbabu <suvendupaulbabu@gmail.com>
2024-08-20 06:15:48 +0000

    Function Notes

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -28,4 +28,50 @@ function loginUserMSG(userName){
     //return `${userName} logged in`
     return userLoggedIN
 }
-console.log(loginUserMSG("Suvendu Paul"))
+//console.log(loginUserMSG("Suvendu Paul"))
+
+// Function with rest oparetor (...)
+//const priceARR = [200, 500, 600, 45]
+// function getCartPrice(num1, num2, ...cartPrice){
+//     return num1, num2, cartPrice
+// }
+function getCartPrice(...cartPrice){
+    return cartPrice
+}
+const totalPriceOfCart = getCartPrice(200, 500, 600, 45, 99.25)
+//console.log(totalPriceOfCart)
+
+// Pass object reference to the function
+const mySym = Symbol("key1");
+const jsUser = {
+    name : "PUER", 
+    prices : 99, 
+    "full name" : "Brand Nourish PUER", 
+    [mySym] : "myKey2", 
+    age : 18, 
+    location : "kolkata", 
+    email : "puer@brandnourish.in", 
+    isLoggedIn : false, 
+    lastLoginDays : ["Mondal", "Saturday"]
+}
+
+console.log(typeof jsUser === "object")
+
+function getUser(anyObject, objKey){
+    //console.log(`User name is ${anyObject.name} and price is ${anyObject.price}`) 
+
+    //return `User name is ${anyObject.name} and price is ${anyObject.price}`
+
+    console.log('OBJ : ', anyObject)
+    //console.log('OBJ Key : ', anyObject.map(objKey))
+    
+    return typeof anyObject === 'object' && anyObject !== null ? `User name is ${anyObject.name} and price is ${anyObject.price}` : ''
+}
+console.log(getUser(jsUser))
+
+// Pass array reference to the function
+const myArr = [1, 2, 3, 5, 8]
+function getArr(...anyArray){
+    return anyArray[0]
+}
+console.log(getArr(myArr))
