4f8a14acf50ae0e5d2f2ec74bb852e3a9838767b
suvendupaulbabu <suvendupaulbabu@gmail.com>
2024-08-22 18:13:58 +0000

    Function Scope Note

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -12,23 +12,38 @@ if(true){
 //console.log('var', c)
 
 // Function Scope
+// *If function create inside function then it's called closer
 function one(){
     const userName = 'PUER'
     function two(){
         const age = 3
-        console.log(userName)
+        //console.log(userName)
     }
     two()
     //console.log(age)
 }
-console.log(one())
+//console.log(one())
 
 if(true){
     const userName = "Suvendu"
     if(true){
         const password = 123
-        console.log(`Your name is ${userName} and password is ${password}`)
+        //console.log(`Your name is ${userName} and password is ${password}`)
     }
     //console.log('Your passwod: ', password)
 }
 //console.log(userName)
+
+// Normal Function
+// console.log(addOne(8)) => For normal function it's called before initialize/create function
+function addOne(num){
+    return num + 1
+}
+console.log(addOne(8))
+
+// Expression
+// console.log(addTwo(8)) => If function assign to any variable (which is called "Expression") then not able to call variable before initialize/create function
+const addTwo = function(num){
+    return num + 4
+}
+console.log(addTwo(8))
