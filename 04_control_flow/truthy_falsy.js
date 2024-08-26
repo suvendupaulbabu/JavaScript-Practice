dce4a08ed5dc99c6d762e155a9cbe722c65a21b1
suvendupaulbabu <suvendupaulbabu@gmail.com>
2024-08-26 05:36:31 +0000

    Conditional Notes

--- 04_control_flow/truthy_falsy.js
+++ 04_control_flow/truthy_falsy.js
@@ -1,5 +1,5 @@
 // Check condition using Truthy / Falsy value
-const userEmail = {}
+const userEmail = function(){}
 
 // Condition
 if(userEmail){
@@ -19,3 +19,24 @@ if(userEmail){
 07. undefined => undefined is "Falsy" value
 08. NaN => NaN is "Falsy" value
 */
+
+// Truthy Value
+/* 
+01. "0" => If inside semiclone have zero then it's also truthy value
+02. "false" => Inside data type string all are truthy value
+03. " " => If inside semiclone have a space it's also truthy value
+04. []/{} => Empty array/object is also truthy value
+05. function(){} => Empty function is also truthy value
+06. 
+07. 
+08. 
+*/
+
+// How to check empty object
+const emptyOBJ = {}
+//console.log(Object.keys(emptyOBJ))
+
+// Here using Object.keys return an array, and then check return array length and type === 0 or not
+if(Object.keys(emptyOBJ).length === 0){
+    console.log('Object is empty')
+}
